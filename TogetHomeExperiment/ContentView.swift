0e5d49f0ab873d2ed6c354cdf9c3b36fc942ae7e
Duvle <itdiceofficial@gmail.com>
2023-04-01 00:50:01 +0900

    최초 생성

--- TogetHomeExperiment/ContentView.swift
+++ TogetHomeExperiment/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "wave.3.right.circle.fill")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Toget Home Experiment")
         }
         .padding()
     }
