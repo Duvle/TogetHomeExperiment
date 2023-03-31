eeabe6b0b9e9fe6d2ad5042c7265db2e4116c895
Duvle <itdiceofficial@gmail.com>
2023-04-01 00:43:23 +0900

    Initial Commit

---
+++ TogetHomeExperiment/TogetHomeExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TogetHomeExperimentApp.swift
+//  TogetHomeExperiment
+//
+//  Created by IT DICE on 2023/04/01.
+//
+
+import SwiftUI
+
+@main
+struct TogetHomeExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
